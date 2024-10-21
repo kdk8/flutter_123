292cf11cc1cc58da62265348cf2cba684f4cad15
kdk8 <kdksantika8@gmail.com>
2024-10-21 18:07:31 +0800

    OVAL PROJECT

--- kadek_project/lib/main.dart
+++ kadek_project/lib/main.dart
@@ -44,34 +44,39 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: [
           Align(
-            alignment: Alignment.centerLeft,
-            child: Text("pembembing Akademik (PA)",style: TextStyle(fontSize:20),),
-            ClipOval(
-              child: Image.network("https://www.google.com/imgres?q=prabowo%20jpg&imgurl=https%3A%2F%2Fwww.benarnews.org%2Findonesian%2Fopini%2Fopini-prabowo-ke-as-10152020224352.html%2F201015_ID_Prabowo_1000.jpg%2F%40%40images%2Fimage&imgrefurl=https%3A%2F%2Fwww.benarnews.org%2Findonesian%2Fopini%2Fopini-prabowo-ke-as-10152020224352.html&docid=eF8yx_jrTAD_cM&tbnid=kJ2SgP7rBa34WM&vet=12ahUKEwjD1_T6jp2JAxWIUGwGHfLCAQsQM3oECBgQAA..i&w=1000&h=667&hcb=2&ved=2ahUKEwjD1_T6jp2JAxWIUGwGHfLCAQsQM3oECBgQAA",
-              height:200,
+            alignment: Alignment.centerLeft, // Menempatkan teks di sebelah kiri
+            child: Text(
+              "Pembimbing Akademik (PA)",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          ClipOval(
+            child: Image.network(
+              "https://www.google.com/imgres?q=gambar%20format%20jpg&imgurl=https%3A%2F%2Ffileinfo.com%2Fimg%2Fss%2Fxl%2Fjpg_44-2.jpg&imgrefurl=https%3A%2F%2Ffileinfo.com%2Fextension%2Fjpg&docid=0OJPsWXk41YY4M&tbnid=c9N72fqs210o-M&vet=12ahUKEwjcwoTwzp6JAxUoR2wGHa7jGIEQM3oFCIABEAA..i&w=2400&h=1599&hcb=2&ved=2ahUKEwjcwoTwzp6JAxUoR2wGHa7jGIEQM3oFCIABEAA",
+              height: 200, // Disesuaikan agar proporsional dengan bentuk bulat
               width: 200,
-              fit: BoxFit.cover,
-              ),
+              fit: BoxFit.cover, // Menyesuaikan gambar dengan bingkai bulat
             ),
-            Row(
-              children: [
-                Icon(Icon.person),
-                Text("I Kadek Santika"),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.email),
-                Text("kdksantika8@gmail.com"),
-              ],
-            ),
+          ),
+          Row(
+            children: [
+              Icon(Icons.person),
+              Text(" I Kadek Santika"),
+            ],
+          ),
+          Row(
+            children: [
+              Icon(Icons.email),
+              Text("kadeksantika1010@gmail.com"),
+            ],
+          ),
         ],
-      )
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ),
-    );
-  }
+     ),
+);
+}
 }

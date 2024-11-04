8eeed85eba6ab0c5d2f57d108b15ec96cd1cb873
kdk8 <kdksantika8@gmail.com>
2024-11-04 22:23:19 +0800

    uts biodata_flutter

--- kadek_project/lib/main.dart
+++ kadek_project/lib/main.dart
@@ -1,121 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Biodata'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: NetworkImage('https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedan.tribunnews.com%2F2022%2F08%2F31%2F7-contoh-gambar-yang-mudah-ditiru-berikut-tips-menggambarnya%3Fpage%3Dall&psig=AOvVaw0tq6CYxyD6wGbwLmAe2Fdn&ust=1730801813827000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJCf1q25wokDFQAAAAAdAAAAABAE'),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'i Kadek Santika',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Text('NIM: 42230001'),
+              Text('Prodi: FTI (Teknologi Informasi)'),
+              Text('Deskripsi Diri: Saya mahasiswa Undiksha angkatan 2022.'),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          // Sidebar
-          Container(
-            width: 200,
-            color: Colors.blueGrey,
-            child: Column(
-              children: [
-                DrawerHeader(
-                  child: Text(
-                    'MENU',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.blueGrey,
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.home, color: Colors.white),
-                  title: Text('Home', style: TextStyle(color: Colors.white)),
-                ),
-                ListTile(
-                  leading: Icon(Icons.settings, color: Colors.white),
-                  title:
-                      Text('Settings', style: TextStyle(color: Colors.white)),
-                ),
-                ListTile(
-                  leading: Icon(Icons.contact_page, color: Colors.white),
-                  title: Text('Contact', style: TextStyle(color: Colors.white)),
-                ),
-              ],
-            ),
-          ),
-          // Main Content Area (Expanded)
-          Expanded(
-            child: Column(
-              children: [
-                // Header
-                Container(
-                  height: 50,
-                 color: Colors.blue,
-                  child: Center(
-                    child: Text(
-                      'Data Psikolog',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                      ),
-                    ),
-                  ),
-                ),
-                // Content
-                Expanded(
-                  child: Container(
-                    color: Colors.white,
-                    child: Center(
-                      child: Text(
-                        'DATA',
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }
